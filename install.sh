82b168ad7bb442fea89b84ac9c929a073034c1b4
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-10-04 21:40:36 +0900

    Change `install.sh` a little

--- install.sh
+++ install.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+function name()
+{
+    # info ok/error/info/section
+    status=$1
+    text=$2
+    if [ $status == 'ok'] 
+    then
+        echo \e "[  OK  ] $text"
+    fi
+    if condition 
+    then
+        echo \e "[ ERR! ] $text"
+    fi
+    if condition 
+    then
+        echo \e "[ INFO ] $text"
+    fi
+    if condition 
+    then
+        echo \e "__--== $text ==--__"
+    fi
+}
