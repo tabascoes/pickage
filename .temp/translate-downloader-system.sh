f091df7d958ebc0c1ce94a52e373409985bbd1bd
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-10-06 20:35:01 +0900

    Changed translation file and added `.tmp` file

---
+++ .temp/translate-downloader-system.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O https://raw.githubusercontent.com/tabascoes/pickage/main/translate/translatefile.test
--- install.sh
+++ install.sh
@@ -1,23 +1,50 @@
 #!/bin/bash
-function name()
+# Exec format might be
+# curl XXX | sudo bash jp/en
+lang=$1
+
+function ie()
 {
-    # info ok/error/info/section
+    # info o/e/i/s text
     status=$1
     text=$2
-    if [ $status == 'ok' ] 
+    if [ $status == 'o' ] 
     then
         echo \e "[  OK  ] $text"
     fi
-    if condition 
+    if [ $status == 'e' ] 
     then
         echo \e "[ ERR! ] $text"
     fi
-    if condition 
+    if [ $status == 'i' ] 
     then
         echo \e "[ INFO ] $text"
     fi
-    if condition 
+    if [ $status == 's' ] 
     then
         echo \e "__--== $text ==--__"
     fi
 }
+
+ie "s" "You can CANCEL installer CTRL + C"
+ie "i" "installer will install in /usr/local/bin/pickage"
+ie "i" "Waiting 5 seconds"
+sleep 5
+ie "i" "No interrupt, CONTINUE"
+
+ie "s" "Starting/Configuring installer"
+ie "i" "Checking for authorization"
+sudo echo \e "Authorized"
+ie "i" "Making system directory"
+mkdir /usr/local/bin/pickage
+ie "o" "Finished"
+ie "i" "Making change for directory"
+cd /usr/local/bin/pickage
+sudo chmod 110 /usr/local/bin/pickage
+ie "o" "Finished"
+
+ie "s" "Installing translation file"
+ie "i" "installer will install $lang package"
+mkdir translate
+cd tlanslate
+
